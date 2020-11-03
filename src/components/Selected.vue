ab2823811140d368d8344a86520c1e92fba3dba3
Salifyanji Malunga <salifyanji.malunga@northrise.net>
2020-11-03 15:19:17 +0200

    Intial commit of a  dynamic v-select

---
+++ src/components/Selected.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="SelectItem">
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-select
+          :items="items"
+          label="Standard"
+          @change="selectedButton"
+          v-model="selected"
+          dense
+        >
+        </v-select>
+      </v-col>
+    </v-row>
+    <div v-if="selected === 'Red Button'">
+      <v-col cols="12" md="4">
+        <v-btn small color="error">Red button</v-btn>
+      </v-col>
+    </div>
+    <div v-if="selected === 'Green Button'">
+      <v-col cols="12" md="4">
+        <v-btn small color="success">Green button</v-btn>
+      </v-col>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class SelectItem extends Vue {
+  items: Array<string> = ["Red Button", "Green Button"];
+
+  //the selected variable is declared to reset the v-select options
+  private selected = "";
+  //method
+  selectedButton() {
+    console.log("test");
+  }
+}
+</script>
